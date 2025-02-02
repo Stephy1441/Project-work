dc4e5a2e6b64b2e982a4335580662e7d930dc2b5
emefa <ayivorstephaniesea@gmail.com>
2025-02-02 13:46:10 +0000

    pf-l2-1.0

---
+++ pf-l2-1.0/main.py
@@ -0,0 +1,14 @@
+shopping_list = []
+
+# Prompt the user for three items and append each to the list
+item_1 = input("Enter the first item:")
+shopping_list.append(item_1)
+
+item_2 = input("Enter the second item:")
+shopping_list.append(item_2)
+
+item_3 = input("Enter the third item:")
+shopping_list.append(item_3)
+
+# Print the shopping_list data structure
+print(shopping_list)
